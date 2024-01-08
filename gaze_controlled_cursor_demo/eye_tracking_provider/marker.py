1ca6896d6bbb055152910ffbbf67e1903e4495d4
Marc Tonsen <mar@pupil-labs.com>
2024-01-08 18:06:28 +0100

    Started draft for marker visualization

--- gaze_controlled_cursor_demo/eye_tracking_provider/marker.py
+++ gaze_controlled_cursor_demo/eye_tracking_provider/marker.py
@@ -9,41 +9,40 @@ from pupil_labs.real_time_screen_gaze import marker_generator
 
 
 class Marker(QLabel):
-    def __init__(self, marker_id, alignment, size=250, brightness=170):
+    def __init__(self, marker_id, alignment, brightness=170):
         super().__init__()
         self.id = marker_id
-        self.marker_size = size
         self.brightness = brightness
 
         self.setAlignment(alignment)
         self._pixmap = self._createMarker()
         self.setPixmap(self._pixmap)
 
-        # self.setFixedSize(self.marker_size, self.marker_size)
-        # self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        # self.setWindowFlag(Qt.WindowTransparentForInput)
+        self.setStyleSheet("background-color: cyan;")
 
     def paintEvent(self, event):
         res = super().paintEvent(event)
         painter = QPainter(self)
         painter.fillRect(self.rect(), QColor(0, 0, 0, 255 - self.brightness))
-
+        painter.drawPixmap(self.rect(), self._pixmap)
         return res
 
     def resizeEvent(self, event: QResizeEvent) -> None:
         self.setPixmap(self._pixmap.scaled(self.size(), Qt.KeepAspectRatio))
-
-        return super().resizeEvent(event)
+        print(self.id, self.size(), self.get_marker_verts())
 
     def get_marker_verts(self):
         """Returns the markers coordinates in global screen space."""
+        width = self.width()
+        height = self.height()
+        assert width == height or (width == 640 and height == 480)
 
         # The marker has a white border which should be ignored when calculating the vertex positions.
         rect = self.rect() - QMargins(
-            self.marker_size / 8,
-            self.marker_size / 8,
-            self.marker_size / 8,
-            self.marker_size / 8,
+            width / 8,
+            width / 8,
+            width / 8,
+            width / 8,
         )
         verts = [
             rect.topLeft(),
--- gaze_controlled_cursor_demo/marker_overlay.py
+++ gaze_controlled_cursor_demo/marker_overlay.py
@@ -1,10 +1,54 @@
+from typing import Optional
 from PySide6.QtCore import *
+from PySide6.QtCore import Qt
 from PySide6.QtGui import *
-from PySide6.QtGui import QKeyEvent, QResizeEvent
+from PySide6.QtGui import QKeyEvent, QPaintEvent, QResizeEvent
 from PySide6.QtWidgets import *
+from PySide6.QtWidgets import QWidget
 
 from eye_tracking_provider import Marker
 import utils
+
+
+class MarkerContainer(QWidget):
+    def __init__(self, marker_id, alignment):
+        super().__init__()
+        self.alignment = alignment
+        self.border_width = 5
+
+        self.marker = Marker(marker_id, alignment)
+        layout = QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+        layout.addWidget(self.marker, alignment=alignment)
+        self.setLayout(layout)
+        # self.marker.setParent(self)
+
+    def paintEvent(self, event: QPaintEvent) -> None:
+        super().paintEvent(event)
+
+        painter = QPainter(self)
+        size = self.marker.pixmap().rect().size()
+        pos = self.marker.mapToParent(self.marker.rect().topLeft())
+        rect = QRect(pos, size)
+
+        if self.alignment == Qt.AlignLeft | Qt.AlignTop:
+            rect += QMargins(0, 0, self.border_width, self.border_width)
+        elif self.alignment == Qt.AlignLeft | Qt.AlignBottom:
+            rect += QMargins(0, self.border_width, self.border_width, 0)
+        elif self.alignment == Qt.AlignRight | Qt.AlignTop:
+            rect += QMargins(self.border_width, 0, 0, self.border_width)
+        elif self.alignment == Qt.AlignRight | Qt.AlignBottom:
+            rect += QMargins(self.border_width, self.border_width, 0, 0)
+
+        painter.fillRect(rect, QColor(255, 0, 0, 255))
+
+    def resizeEvent(self, event: QResizeEvent) -> None:
+        edge = min(self.width(), self.height()) * 0.95
+        self.marker.setFixedSize(edge, edge)
+
+    def get_marker_verts(self):
+        return self.marker.get_marker_verts()
 
 
 class MarkerOverlay(QWidget):
@@ -34,19 +78,19 @@ class MarkerOverlay(QWidget):
         #         layout.addWidget(m, j, i, 1, 1)
         #         self.markers.append(m)
 
-        m = Marker(0, Qt.AlignLeft | Qt.AlignTop)
+        m = MarkerContainer(0, Qt.AlignLeft | Qt.AlignTop)
         layout.addWidget(m, 0, 0, 3, 2)
         markers.append(m)
 
-        m = Marker(1, Qt.AlignLeft | Qt.AlignBottom)
+        m = MarkerContainer(1, Qt.AlignLeft | Qt.AlignBottom)
         layout.addWidget(m, 13, 0, 3, 2)
         markers.append(m)
 
-        m = Marker(2, Qt.AlignRight | Qt.AlignTop)
+        m = MarkerContainer(2, Qt.AlignRight | Qt.AlignTop)
         layout.addWidget(m, 0, 18, 3, 2)
         markers.append(m)
 
-        m = Marker(3, Qt.AlignRight | Qt.AlignBottom)
+        m = MarkerContainer(3, Qt.AlignRight | Qt.AlignBottom)
         layout.addWidget(m, 13, 18, 3, 2)
         markers.append(m)
 
