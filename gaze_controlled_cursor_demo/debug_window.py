9fbe3f164f16433d6d70c380eb4eaa7b8eb658de
Marc Tonsen <mar@pupil-labs.com>
2024-01-08 15:33:04 +0100

    Simplify code

--- gaze_controlled_cursor_demo/debug_window.py
+++ gaze_controlled_cursor_demo/debug_window.py
@@ -1,14 +1,13 @@
-import sys
-
 from PySide6.QtCore import *
 from PySide6.QtGui import *
-from PySide6.QtGui import QKeyEvent, QResizeEvent
 from PySide6.QtWidgets import *
 
 
 class DebugWindow(QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Debug Window - Scene Camera")
+
         self.setFixedSize(800, 600)
         layout = QVBoxLayout()
 
@@ -16,11 +15,7 @@ class DebugWindow(QWidget):
         self.img_label.setText("")
         self.img_label.setScaledContents(True)
         self.img_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-
         layout.addWidget(self.img_label)
-
-        # self.img_label.setAlignment(Qt.AlignCenter)
-        # self.img_label.setStyleSheet("background:transparent;")
 
         self.setLayout(layout)
 
@@ -31,11 +26,3 @@ class DebugWindow(QWidget):
         qImg = QImage(scene.data, width, height, bytesPerLine, QImage.Format_RGB888)
         pixmap = QPixmap(qImg)
         self.img_label.setPixmap(pixmap)
-        # self.img_label.resize(pixmap.width(), pixmap.height())
-        # self.resize(pixmap.width(), pixmap.height())
-
-    def paintEvent(self, event):
-        pass
-
-    def resizeEvent(self, event: QResizeEvent) -> None:
-        pass
